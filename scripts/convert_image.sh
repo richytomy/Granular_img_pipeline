918f9070b50617b51e2a04b48c7c0cc2ed84dd52
Richy Tomy <40405977+richytomy@users.noreply.github.com>
2019-02-16 12:56:03 -0700

    Update convert_image.sh

--- scripts/convert_image.sh
+++ scripts/convert_image.sh
@@ -2,6 +2,6 @@
 # This script is intended to convert raw jpeg images to (224,224,3)
 
 echo "Starting images conversion"
-for dir in /../imgs_de/*; do
-        echo $dir;
-        done
+#for dir in /../imgs_de/*; do
+ #       echo $dir;
+  #      done
